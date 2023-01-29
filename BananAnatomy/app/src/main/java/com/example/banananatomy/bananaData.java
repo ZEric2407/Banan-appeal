0e4b0e17deae412dcb3e34a06515b3d143700ad1
ZEric2407 <112508698+ZEric2407@users.noreply.github.com>
2023-01-29 01:12:19 -0500

    Updated perms, created banana class

---
+++ BananAnatomy/app/src/main/java/com/example/banananatomy/bananaData.java
@@ -0,0 +1,11 @@
+package com.example.banananatomy;
+
+public class bananaData {
+    private int ripeness;
+    private int expiresIn;
+    public bananaData(int ripeness){
+        if (ripeness == 0){
+            
+        }
+    }
+}
